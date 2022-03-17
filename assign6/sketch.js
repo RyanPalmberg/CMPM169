08eb8b2910602e2ff2810ad76b8b7fe03e1a7da5
RyanP <rpalmber@ucsc.edu>
2022-03-16 19:19:17 -0700

    final touches

--- assign6/sketch.js
+++ assign6/sketch.js
@@ -1,13 +1,19 @@
-/*
+/*/*
+original by:
 Gurkirat Saini
 gssaini
 Rats
 
-Instructions: Simply move the mouse across the screen like a slider and enjoy. 
+remixed by:
+Ryan Palmberg
+rpalmber
+
+Instructions: Chase the rat with the cheese to increase the volume!
 
 Music: https://www.youtube.com/watch?v=vdVnnMOTe3Q
 */
-var rectLocation;
+
+let x1, y1;
 function preload() {
   rat = loadImage('assets/rat.png');
   mixtape = loadSound('assets/mixtape.mp3');
@@ -17,42 +23,37 @@ function preload() {
 
 function setup() {
   createCanvas(600, 400);
-  // imageMode(CENTER);
   mixtape.loop();
   ratAmp = new p5.Amplitude();
-  rectLocation = createVector(width/2,height/2);
+  // x1 = 300;
+  // y1 = 200;
+  // x1 = mouseX;
+  // y1 = mouseY;
 }
 
 function draw() {
-  
-  var target = createVector(mouseX,mouseY);
-  var distance = target.dist(rectLocation);
-  var mappedDistance = map(distance, 100, 0, 1.5, 0.5);
-  target.sub(rectLocation);
-  target.normalize();
-  target.mult(mappedDistance);
-  rectLocation.add(target);
-  
   background(bg);
-  //mouseX controls volume
-  // let volume = map(mouseX, 0, width, 0, 1);
-  let volume = map(distance, 0, width, 1, 0);
+  x1 = mouseX;
+  y1 = mouseY;
+  // image(cheese, x1, y1, 80, 80)
+  // translate(width/2, height/2);
+  image(cheese, x1, y1, 80, 80)
+  let x3 = (200 * sin(frameCount * 0.05))+width/2;
+  let y3 = (150 * cos(frameCount * 0.05))+height/2;
+  // x1 = mouseX;
+  // y1 = mouseY;
+
+  let trail = dist(x1, y1, x3, y3);
+  // line(x1, y1, x3, y3)
+
+  let volume = map(trail, 0, width, 1, 0);
   volume = constrain(volume, 0, 1);
   mixtape.amp(volume);
-  // controls the rat in relation to the mouse
-  let ratWidth = 60;
-  // let ratHeight = ratAmp.getLevel() * height + ratWidth*2/3;
-  let ratHeight = ratAmp.getLevel() * ratWidth * 15;
-  
-  image(cheese, mouseX, mouseY, 80, 80)
-  translate(rectLocation.x, rectLocation.y);
-  // image(rat, mouseX-ratWidth/2, height/2-ratHeight/2, ratWidth, ratHeight);
-  image(rat, 0, 0, ratWidth, ratHeight);
-  
-  
+  let ratWidth = 100;
+  let ratHeight = ratAmp.getLevel() * ratWidth * 10;
 
-  
-  // rect(rectLocation.x, rectLocation.y, 10,10);
-  // image(rat, rectLocation.x * mouseX-ratWidth/2, rectLocation.y * height/2-ratHeight/2, ratWidth, ratHeight);
-  
+  push();
+  // translate(width/2, height/2);
+  image(rat, x3, y3, ratWidth, ratHeight);
+  pop();
 }
